049dd7badfcbaf07729d2fcab3fa797d6e5a1b87
Rouxel Armel <arouxel@outlook.fr>
2019-07-18 19:08:28 +0200

    mysql com
    dcf

--- index.js
+++ index.js
@@ -9,9 +9,33 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static(__dirname + '/public'));
 app.use(session({secret: 'todotopsecret'}))
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'ydogbe'
+});
+
+// ========================================== FONCTION MYSQL =============================================
+
+function addUser (username, password, adresse, newsletter, email) {
+    var req = `INSERT INTO users (id, name, password, subscribe_date, adresse, newsletter, email) 
+               VALUES (NULL, "${username}", "${password}", NOW(), "${adresse}", "${newsletter}", "${email}")`;
+    connection.query(req, function(err, rows, fields) {
+        if (err) throw err;
+        console.log(rows);
+    });
+}
+
 // ======================================== FONCTIONS SIMPLES ============================================
 
-function logIn(req, username, pasword) {
+function subscribe(req, username, password, adresse, newsletter, email) {
+    addUser(username, password, adresse, newsletter,email);
+    logIn(req, username, password)
+}
+
+function logIn(req, username, password) {
 	req.session.username = username;
 	req.session.logged = true;
 }
@@ -51,12 +75,19 @@ app.get('/', function(req, res) {
 
 .post('/login', urlencodedParser, function(req, res) {
     logIn(req, req.body.username, req.body.password);
-    res.redirect('/hub');
+    res.redirect('/mainpage');
 })
 
 .post('/sign-up', urlencodedParser, function(req, res) {
-	logIn(req, req.body.username, req.body.password);
-    res.redirect('/hub'); 
+    var adresse = `${req.body.adresse} ${req.body.city} ${req.body.country}`; // L'adresse est l'assemblage de la ville, pays et rue.
+	
+    // Si la case n'est pas coché -> no
+    if (!req.body.newsletter) {
+        req.body.newsletter = "no";
+    }
+    console.log(req.body.newsletter);
+    subscribe(req, req.body.name, req.body.password, adresse, req.body.newsletter, req.body['e-mail']);
+    res.redirect('/'); 
 })
 
 .post('/logout', urlencodedParser, function(req, res) {
--- mysql.js
+++ mysql.js
@@ -4,14 +4,15 @@ var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
 	password : '',
-	database : 'database_test'
+	database : 'ydogbe'
 });
 
-var sqlDatabase = 'CREATE DATABASE dbtest';
+var sqlTable = 'SELECT * FROM users';
+var addUser = 'INSERT INTO users (id, name, password, subscribe_date) VALUES (NULL, "John", "Casserole", NOW())';
 
-connection.query(sqlTable, function(err, rows, fields) {
+connection.query(addUser, function(err, rows, fields) {
 	if (err) throw err;
-	console.log('Database created');
+	console.log(rows);
 });
 
 connection.end();
