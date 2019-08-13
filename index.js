2790660e752a6f2171bb4013a895ac1480ff55b8
Rouxel Armel <arouxel@outlook.fr>
2019-08-13 20:50:19 +0200

    login + subscribe

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var app = express();
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(session({ 
+        name: "ydogbe",
         secret: 'keyboard cat', 
         cookie: { maxAge: 60000 },
         resave: false,
@@ -15,6 +16,7 @@ app.use(session({
     })
 );
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
@@ -24,9 +26,90 @@ var connection = mysql.createConnection({
     database : 'ydogbe'
 });
 
-// ======================================== FONCTIONS SIMPLES ============================================
+// ================================================ ROUTES ===============================================
 
-function subscribe(req, res, username, password, adresse, newsletter, email) {
+app.get('/', function(req, res) {
+    res.redirect('/mainpage');
+})
+
+.get('/log', function(req, res) {
+    res.render('log.ejs', {session: req.session});
+})
+
+.get('/quit', function(req, res) {
+    req.session.username = "";
+    req.session.logged = false;
+    req.session.alert = "logout";
+    res.redirect('back');
+})
+
+.get('/subscribe', function(req, res) {
+	res.render('subscribe.ejs', {session: req.session});
+})
+
+.get('/:username/info', function(req, res) {
+    res.render('account_info.ejs', {username: req.params.username, session: req.session});
+})
+
+.get('/product/:id', function(req, res) {
+    res.render('product.ejs', {id: req.params.id, session: req.session});
+})
+
+.get('/mainpage', function(req, res) {
+    // On remet les variables de session par defaut si besoin
+	res.render('mainpage.ejs', {session: req.session});
+})
+
+// ============================================= POST ===================================================
+
+.post('/login', urlencodedParser, function(req, res) {
+    var password = req.body.password;
+    var email = req.body.email;
+
+    // On construit la requête et on l'envoit
+    var requestMysql = `SELECT * FROM users WHERE email='${email}'`;
+    connection.query(requestMysql, function(err, rows, fields) {
+        if (err) throw err;
+        var user = rows[0]; // on prend seulement 1 utilisateur (le seul en théorie)
+
+        // Si l'email donné est bien enregistré ...
+        if (user) {
+            bcrypt.compare(password, user.password, function(err, response) {
+                // Si les mots de passe correspondent ...
+                if (response) {
+                    req.session.username = user.name;
+                    req.session.logged = true;
+                    req.session.alert = "login";
+                    console.log("logged !");
+                    res.redirect('back');
+                } 
+                else {
+                    req.session.error = "Bad password";
+                    console.log("faux mot de passe"); // Les id ne correspondent pas
+                    res.send('badPassword');
+                }
+            });
+        }
+        else {
+            req.session.error = "Unknown email";
+            console.log("aucun utilisateur"); // Les id ne correspondent pas
+            res.send('badEmail');
+        }
+    }); 
+})
+
+.post('/sign-up', urlencodedParser, function(req, res) {
+    var adresse = req.body.adresse; // L'adresse est l'assemblage de la ville, pays et rue.
+	var username = req.body.name;
+    var password = req.body.password;
+    var newsletter = req.body.newsletter;
+    var email = req.body.email;
+    
+    // Si la case n'est pas coché -> no
+    if (!newsletter) {
+        newsletter = "no";
+    }
+
     // On hash le mot de passe à enregistrer dansla DB
     bcrypt.hash(password, 10, function(err, hashedPassword) {
         // On construit la requête et on l'envoit (avec check d'erreur)
@@ -41,97 +124,29 @@ function subscribe(req, res, username, password, adresse, newsletter, email) {
             if (!rows.length) {
                 connection.query(addUser, function(err, rows, fields) {
                     if (err) throw err;
-                    console.log("Utilisateur ajouté");
-                    logIn(req, email, password)  // On log l'utilisateur ensuite
-                    res.redirect('/');  // on redirige vers le menu
+                    req.session.alert = "signup";
+                    console.log("sign up !");
+                    res.redirect('back'); // on recharge la page
                 });
             }
             else {
                 console.log("L'adresse mail est déjà utilisé");
-                res.redirect('back');  // on reload la page
-                req.session.error = "L'adresse mail est déjà utilisé"; // on stock l'erreur dans la seesion (pour créer une notif)
+                //req.session.alert = "email already used"; // on stock l'erreur dans la seesion
+                res.send('badEmail');
             }
         });
     });
-}
-
-function logIn(req, email, password) {
-    // On construit la requête et on l'envoit
-    var requestMysql = `SELECT * FROM users WHERE email='${email}'`;
-    connection.query(requestMysql, function(err, rows, fields) {
-        if (err) throw err;
-        var user = rows[0]; // on prend seulement 1 utilisateur (le seul en théorie)
-
-        // Si l'email donné est bien enregistré ...
-        if (user) {
-            bcrypt.compare(password, user.password, function(err, res) {
-                // Si les mots de passe correspondent ...
-                if (res) {
-                    req.session.username = user.username;
-                    req.session.logged = true; 
-                    console.log("logged !");
-                }  
-            });
-        }
-        else {
-            console.log("aucun utilisateur");
-            // Les id ne correspondent pas
-        }
-    });    
-}
-
-function logOut(req, email) {
-	req.session.username = '';
-	req.session.logged = false;
-}
-
-// ================================================ ROUTES ===============================================
-
-app.get('/', function(req, res) {
-    res.redirect('/mainpage');
-})
-
-.get('/log', function(req, res) {
-    res.render('log.ejs');
-})
-
-.get('/subscribe', function(req, res) {
-	res.render('subscribe.ejs');
-})
-
-.get('/:username/info', function(req, res) {
-    res.render('account_info.ejs', {username: req.params.username});
-})
-
-.get('/product/:id', function(req, res) {
-    res.render('product.ejs', {id: req.params.id});
-})
-
-.get('/mainpage', function(req, res) {
-	res.render('mainpage.ejs');
-})
-
-// ============================================= POST ===================================================
-
-.post('/login', urlencodedParser, function(req, res) {
-    logIn(req, req.body.email, req.body.password);
-    res.redirect('back');
-})
-
-.post('/sign-up', urlencodedParser, function(req, res) {
-    var adresse = `${req.body.adresse} ${req.body.city} ${req.body.country}`; // L'adresse est l'assemblage de la ville, pays et rue.
-	
-    // Si la case n'est pas coché -> no
-    if (!req.body.newsletter) {
-        req.body.newsletter = "no";
-    }
-    console.log(req.body.newsletter);
-    subscribe(req, res, req.body.name, req.body.password, adresse, req.body.newsletter, req.body.email);
 })
 
 .post('/logout', urlencodedParser, function(req, res) {
-    logOut(req, req.body.username);
+    req.session.username = '';
+    req.session.logged = false;
     res.redirect('back');
+})
+
+.post('/resetNotif', urlencodedParser, function(req, res) {
+    req.session.alert = false;
+    res.end();
 });
 
 // ========================================================================================================
