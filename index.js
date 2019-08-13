5ba9e04c48915f041896981ca78377b2fc626db5
Rouxel Armel <arouxel@outlook.fr>
2019-08-13 22:45:59 +0200

    change mysql User

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ app.set('view engine', 'ejs');
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'root',
-    password : '',
+    user     : 'hutsku',
+    password : 'please',
     database : 'ydogbe'
 });
 
