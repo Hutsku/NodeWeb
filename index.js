0cf5871faa119aa6690fbee1601ed9412fd62b67
Rouxel Armel <arouxel@outlook.fr>
2020-08-13 16:09:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,17 +36,6 @@ var options = {
 // get new instance of the client
 var vault = require("node-vault")(options);
 
-// init vault server
-vault.init({ secret_shares: 1, secret_threshold: 1 })
-.then(function (result) {
-    var keys = result.keys;
-    // set token for all following requests
-    vault.token = result.root_token;
-    // unseal vault server
-    return vault.unseal({ secret_shares: 1, key: keys[0] })
-})
-.catch(console.error);
-
 vault.read('secret/hello')
 .then(function(data) {
     console.log(data);
